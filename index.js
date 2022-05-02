cfffa60bc6a6442bc8e662cd81510d79d7a65ce6
ParejaOnPurpose <pamm_jackson@yahoo.com>
2022-05-01 22:51:26 -0400

    Added javascript event listeners and document.get input value

---
+++ index.js
@@ -0,0 +1,6 @@
+//Get weather on DOM load
+document.addEventListener('DOMContentLoaded; getWeather');
+//Change locatio event
+document.getElementById('w-change-btn').addEventListener('click',(e)) => {
+  const city = document.getElementById('city').ariaValueMaxconst state = document.getElementById('state').value
+}
